bc1c9c82a32f060e184dfd113be28edd7e224e7d
Blaž Česnik <cesnik.b@gmail.com>
2017-01-18 20:27:23 +0100

    Izboljsano dodajanje projekta(dodan opis algoritma)

--- ALGatorWeb-master/projects/views.py
+++ ALGatorWeb-master/projects/views.py
@@ -1,15 +1,18 @@
 # PROBLEMS
+import os
 
+from django.core.files import File
 from django.template.defaulttags import register
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
 from django.http import HttpResponse
 from os import system
 from vision import views
 
 from Classes.FolderScraper import FolderScraper  # scrapes different JSON files and puts them together in objects.
-
+from Classes.GlobalConfig import GlobalConfig
+import json
 @login_required
 def projects(request):
     print()
@@ -31,10 +34,23 @@ def newproject(request):
         pdescription=request.POST.get('pdescription')
         sdescription=request.POST.get('sdescription')
         aname=request.POST.get('aname')
-        alg = str(aname) if aname else 'Testing algorihtm'
-        print(alg)
-        print(pname)
-        system("java algator.Admin -ca " + pname + " " + alg)
+        algdescription=request.POST.get('algdescription')
+        algsdescription=request.POST.get('algsdescription')
+
+        if not check_if_project_exists(pname) and not check_if_algorithm_exists(pname,aname):
+            system("java algator.Admin -ca " + pname + " " + aname)
+
+        proj_conf = return_proj_conf_file(pname)
+        alg_conf = return_alg_conf_file(pname,aname)
+        description_file = open(proj_conf, 'r').read()
+        json_description = json.loads(description_file)
+        json_description["Project"]["Author"]=request.user.username
+        json_description["Project"]["Description"]=pdescription
+        with open(proj_conf, "w") as jsonFile:
+            jsonFile.write(json.dumps(json_description))
+        print(json_description)
+        return redirect('/projects/pdetails?project=' + pname)
+#/projects/pdetails?project=iPhone
 #        try:
 #            projectConfig = views.get_project_config(pname)
 #            projects.append(projectConfig["Name"])
@@ -46,6 +62,26 @@ def newproject(request):
         'newproject.html',
         context_instance=RequestContext(request)
     )
+
+def return_proj_conf_file(project):
+    data_root = GlobalConfig().data_root_path
+    return "{0}/PROJ-{1}/proj/{1}.atp".format(data_root, project)
+
+def return_alg_conf_file(proj, alg):
+    data_root = GlobalConfig().data_root_path
+    return "{0}/PROJ-{1}/algs/ALG-{2}/{2}.atal".format(data_root,proj, alg)
+
+def check_if_project_exists(proj):
+    data_root = GlobalConfig().data_root_path
+    if os.path.exists("{0}/PROJ-{1}".format(data_root,proj)):
+        return True
+    return False
+
+def check_if_algorithm_exists(proj,alg):
+    data_root = GlobalConfig().data_root_path
+    if os.path.exists("{0}/PROJ-{1}/algs/ALG-{2}".format(data_root,proj,alg)):
+        return True
+    return False
 
 @login_required
 def pdetails(request):
